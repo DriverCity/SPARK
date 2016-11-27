233dcd767c108d70c7a52370796caec1a33aad72
Seao <wolzaurelien@gmail.com>
2016-11-27 15:05:26 +0000

    Add Node.js part

---
+++ src/smart_meter/src/BLENode/src/main.js
@@ -0,0 +1,44 @@
+var bleno = require('bleno');
+
+var BlenoPrimaryService = bleno.PrimaryService;
+
+var SmartCharacteristic = require('./characteristic');
+
+console.log('bleno - ble peripheral');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('smart-meter', ['ec00']);
+  }   
+  else {
+    if(state === 'unsupported'){
+      console.log("NOTE: BLE and Bleno configurations not enabled on board, see README.md for more details...");
+    }
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new SmartCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
+
+bleno.on('accept', function(clientAddress) {
+    console.log("Accepted Connection with Client Address: " + clientAddress);
+});
+
+bleno.on('disconnect', function(clientAddress) {
+    console.log("Disconnected Connection with Client Address: " + clientAddress);
+});
