5d1f84fdb63305656f9c06f4ecabab9dcf42e900
Joona Luoma <jkvluoma@gmail.com>
2017-01-01 17:57:43 +0000

    Gather GCS blob by event date

--- src/cloud/cloud_storage_io.py
+++ src/cloud/cloud_storage_io.py
@@ -1,5 +1,6 @@
 import json
 from google.cloud import storage
+from utils import TimeUtils
 
 
 class CloudStorageIO():
@@ -7,7 +8,7 @@ class CloudStorageIO():
     def __init__(self):
         storage_client = storage.Client()
         bucket = storage.Bucket(storage_client, 'parking-event-store')
-        self.blob = storage.Blob('parking-event-blob', bucket) # TODO: set blob name by date
+        self.blob = storage.Blob('parking-event-blob-' + TimeUtils.get_local_datestamp(), bucket)
 
     def upload_json_to_parking_event_store(self, json_values):
         # TODO: compress stored json
--- src/cloud/utils.py
+++ src/cloud/utils.py
@@ -4,14 +4,23 @@ from uuid import uuid4
 
 class TimeUtils():
 
+    locale = 'Europe/Helsinki'
+
+    @staticmethod
+    def __get_local_now():
+        return datetime.datetime.now(pytz.timezone(TimeUtils.locale))
+
     @staticmethod
     def get_epoch_timestamp_plus_seconds(seconds):
         return datetime.datetime.fromtimestamp(time.time() + seconds).strftime('%Y-%m-%d %H:%M:%S')
 
     @staticmethod
     def get_local_timestamp():
-        return datetime.datetime.now(pytz.timezone('Europe/Helsinki')).strftime('%Y-%m-%d %H:%M:%S')
+        return TimeUtils.__get_local_now().strftime('%Y-%m-%d %H:%M:%S')
 
+    @staticmethod
+    def get_local_datestamp():
+        return TimeUtils.__get_local_now().strftime('%Y-%m-%d')
 
 class TokenUtils():
 
