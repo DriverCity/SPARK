b8373bbb6e78d983a3ad4053e4648330f5ef04aa
Seao <wolzaurelien@gmail.com>
2017-01-11 13:52:12 +0200

    Added real-time occupancy rate consideration for the map side

--- src/attendant-app/www/js/controllers/map.js
+++ src/attendant-app/www/js/controllers/map.js
@@ -3,71 +3,127 @@
  *********************************/
 
 app.controller('MapCtrl', function(Firebase, $scope, $state, $cordovaGeolocation, $firebaseArray) {
-  // Geolocation options
+  
+  /**********************
+   * VARIABLES
+   *********************/
+
+  $scope.map = null;
+
+  /**********************
+   * OVERLAY AREA
+   *********************/
+
+  var areaDisplayed = [];
+  var unknow = "#a2a2a2", low = "#0fe067", medium = "#ff9933", lot = "#e03a0f";
+
+  $scope.defineColor = function(occupancyRate) {
+    if(occupancyRate == "UNKNOW") return unknow;
+    if(parseFloat(occupancyRate) < 0.3) return low;
+    if(parseFloat(occupancyRate) > 0.7) return lot;
+    return medium;
+  };
+
+  $scope.removeFromMap = function(area) {
+    console.log("removed");
+    for(var i=0; i<areaDisplayed.length; i++) {
+      if(areaDisplayed[i].area_number == area) {
+        areaDisplayed[i].setMap(null);
+        areaDisplayed.splice(i, 1);
+      }
+    }
+  }
+
+  $scope.addOnMap = function(child) {
+    console.log("added");
+    var featureCoords = child.geometry.coordinates[0];
+    // Convert coords to Google Map format
+    var parkingCoords = [];
+    for(var i=0;i<featureCoords.length;i++) {
+      parkingCoords.push(
+        {
+          lat:parseFloat(featureCoords[i][1]),
+          lng:parseFloat(featureCoords[i][0])
+        }
+      );
+    }
+    // Adding it on the map
+    var parkingGeometry = new google.maps.Polygon({
+      paths: parkingCoords,
+      strokeColor: $scope.defineColor(child.occupancyRate),
+      strokeOpacity: 0.9,
+      strokeWeight: 2,
+      fillColor: $scope.defineColor(child.occupancyRate),
+      fillOpacity: 0.35,
+      area_number: child.area_number
+    });
+    // Set map
+    parkingGeometry.setMap($scope.map);
+    areaDisplayed.push(parkingGeometry);
+  }
+
+
+  $scope.createOverlay = function() {
+    // Retrieve areas
+    var parkingAreaRef = firebase.database().ref().child('parkingArea');
+    $scope.parkingArea = $firebaseArray(parkingAreaRef);
+
+    $scope.parkingArea.$watch(function(e) {
+      // New child
+      if(e.event == "child_added") {
+        $scope.addOnMap($scope.parkingArea.$getRecord(e.key));
+      }
+      // Updated child
+      else if(e.event == "child_changed") {
+        console.log("changed");
+        // Remove his last state
+        $scope.removeFromMap(e.key);
+        // Add the new value
+        $scope.addOnMap($scope.parkingArea.$getRecord(e.key));
+      }
+    });
+  }
+
+
+  /**********************
+   * GEOLOCATION
+   *********************/
+
   var options = {
     timeout: 10000,
     enableHighAccuracy: true
   };
+
   $scope.located = false;
  
   $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
- 
-    // Position coordinates
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
-    // Define options
+  
+    var LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);    
+
     var mapOptions = {
-      center: latLng,
+      center: LatLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
- 
+
     // Create the map
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    $scope.located = true;
 
     // Adding marker current position
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
       var marker = new google.maps.Marker({
         map: $scope.map,
         animation: google.maps.Animation.DROP,
-        position: latLng
+        position: LatLng
       });
     });
 
-    // Reference of parking open data in Firebase
-    var featureRef = firebase.database().ref().child('parkingArea');
-    // Real-time array for data reference
-    $scope.features = $firebaseArray(featureRef);
-    $scope.features.$watch(function(e) {
-      if(e.event == "child_added") {
-        // Extract added feature
-        var feature = $scope.features[$scope.features.length-1];
-        var featureCoords = feature.geometry.coordinates[0];
+    // Update location status
+    $scope.located = true;
 
-        // Convert coords to Google Map format
-        var parkingCoords = [];
-        for(var i=0;i<featureCoords.length;i++) {
-          parkingCoords.push(
-            {
-              lat:parseFloat(featureCoords[i][1]),
-              lng:parseFloat(featureCoords[i][0])
-            }
-          );
-        }
-       
-        // Adding it on the map
-        var parkingGeometry = new google.maps.Polygon({
-          paths: parkingCoords,
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35
-        });
-        parkingGeometry.setMap($scope.map);
-      }
-    });
+    // Add area
+    $scope.createOverlay();
+  
   }, function(error){
     alert("Unexpected error, could not get location ...");
   });
